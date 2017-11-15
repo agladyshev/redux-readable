957920c86fb38b0c1b67d3ae8906a812ac8bca17
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-11-15 16:50:58 +0300

    feat: Create responsive frame layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import 'typeface-roboto'
+import ButtonAppBar from './ButtonAppBar'
+import AutoGrid from './AutoGrid'
+import LabelBottomNavigation from './LabelBottomNavigation'
+
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  root: {
+    height: '100vh',
+    display: 'flex',
+    'flex-flow': 'column'
+  }
+})
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
+      <div className={classes.root}>
+        <ButtonAppBar/>
+        <AutoGrid/>
+        <LabelBottomNavigation/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withStyles(styles)(App)
---
+++ src/LabelBottomNavigation.js
@@ -0,0 +1,46 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import BottomNavigation, { BottomNavigationButton } from 'material-ui/BottomNavigation';
+import RestoreIcon from 'material-ui-icons/Restore';
+import FavoriteIcon from 'material-ui-icons/Favorite';
+import LocationOnIcon from 'material-ui-icons/LocationOn';
+import FolderIcon from 'material-ui-icons/Folder';
+
+const styles = {
+  root: {
+    flex: '0 1 auto',
+  },
+};
+
+class LabelBottomNavigation extends React.Component {
+  state = {
+    value: 'recents',
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
+        <BottomNavigationButton label="All" value="All" showLabel="true"/>
+        <BottomNavigationButton label="React" value="React" showLabel="true"/>
+        <BottomNavigationButton label="Redux" value="Redux" showLabel="true"/>
+        <BottomNavigationButton label="Udacity" value="Udacity" showLabel="true"/>
+      </BottomNavigation>
+    );
+  }
+}
+
+LabelBottomNavigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LabelBottomNavigation);
