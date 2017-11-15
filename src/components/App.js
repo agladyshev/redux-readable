f7e8eb364c111fdf34e9a98789f0076a1cc9397d
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-11-16 00:14:12 +0300

    feat: Dynamically generate navigation menu

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ import { withStyles } from 'material-ui/styles';
 import { fetchPosts } from '../utils/api'
 import { fetchComments } from '../utils/api'
 
+import { connect } from 'react-redux';
+import { compose } from 'redux'
 
 const styles = theme => ({
   root: {
@@ -22,42 +24,42 @@ const styles = theme => ({
 
 class App extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, categories, posts } = this.props;
     return (
       <Switch>
         <Route exact path='/' render={() => (
           <div className={classes.root}>
             <ButtonAppBar/>
             <AutoGrid/>
-            <LabelBottomNavigation/>
+            <LabelBottomNavigation categories={categories}/>
           </div>
         )}/>
         <Route exact path='/:id' render={() => (
           <div className={classes.root}>
             <ButtonAppBar/>
             <AutoGrid/>
-            <LabelBottomNavigation/>
+            <LabelBottomNavigation categories={categories}/>
           </div>
         )}/>
         <Route exact path='/post/:id' render={() => (
           <div className={classes.root}>
             <ButtonAppBar/>
             <AutoGrid/>
-            <LabelBottomNavigation/>
+            <LabelBottomNavigation categories={categories}/>
           </div>
         )}/>
         <Route exact path='/post/:id/edit' render={() => (
           <div className={classes.root}>
             <ButtonAppBar/>
             <AutoGrid/>
-            <LabelBottomNavigation/>
+            <LabelBottomNavigation categories={categories}/>
           </div>
         )}/>
         <Route exact path='/new' render={() => (
           <div className={classes.root}>
             <ButtonAppBar/>
             <AutoGrid/>
-            <LabelBottomNavigation/>
+            <LabelBottomNavigation categories={categories}/>
           </div>
         )}/>
       </Switch>
@@ -65,4 +67,14 @@ class App extends Component {
   }
 }
 
-export default withStyles(styles)(App)
+function mapStateToProps ({ categories, posts }) {
+  return {
+    categories: categories,
+    posts: posts
+  }
+}
+
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps),
+)(App)
--- src/components/LabelBottomNavigation.js
+++ src/components/LabelBottomNavigation.js
@@ -7,6 +7,10 @@ import BottomNavigation, { BottomNavigationButton } from 'material-ui/BottomNavi
 
 import { Link } from 'react-router-dom'
 
+import { capitalize } from '../utils/helpers'
+import { compose } from 'redux'
+import { withRouter } from 'react-router-dom'
+
 const styles = {
   root: {
     flex: '0 1 auto',
@@ -18,23 +22,42 @@ const styles = {
 
 class LabelBottomNavigation extends React.Component {
   state = {
-    value: 'recents',
+    value: this.props.match.url.length > 1 
+    ? this.props.match.url.substr(1) : this.props.match.url,
   };
 
   handleChange = (event, value) => {
     this.setState({ value });
   };
 
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
+  componentDidMount () {
+    // console.log(this.state.location.pathname);
+  }
 
+  render() {
+    const { classes, categories } = this.props
+    const { value } = this.state
+    console.log(this.state)
+    const navBar =  []
+    console.log(categories)
+    for (const category of categories) {
+      const {name, path} = category
+      const label = capitalize(name)
+      navBar.push(
+        <BottomNavigationButton
+        component={Link}
+        to={path}
+        label={label}
+        value={name}
+        showLabel="true"
+        className={classes.button}
+        key={name} />
+      )
+    }
     return (
       <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
-        <BottomNavigationButton component={Link} to="/" label="All" value="All" showLabel="true" className={classes.button}/>
-        <BottomNavigationButton component={Link} to="/react" label="React" value="React" showLabel="true" className={classes.button}/>
-        <BottomNavigationButton component={Link} to="/redux" label="Redux" value="Redux" showLabel="true" className={classes.button}/>
-        <BottomNavigationButton component={Link} to="/udacity" label="Udacity" value="Udacity" showLabel="true" className={classes.button}/>
+        <BottomNavigationButton component={Link} to="/" label="All" value="/" showLabel="true" className={classes.button}/>
+        {navBar}
       </BottomNavigation>
     );
   }
@@ -42,6 +65,12 @@ class LabelBottomNavigation extends React.Component {
 
 LabelBottomNavigation.propTypes = {
   classes: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(LabelBottomNavigation);
+// export default withStyles(styles)(LabelBottomNavigation);
+
+export default compose(
+  withStyles(styles),
+  withRouter,
+)(LabelBottomNavigation)
