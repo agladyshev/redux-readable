a764d328893b0b1b2ed8301afde2b3ab7c3be20b
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-11-23 17:24:36 +0300

    docs: Add attribution for code snippet

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,7 +3,8 @@ export const capitalize = (str = '') => {
     ? ''
     : str[0].toUpperCase() + str.slice(1)
 }
-
+// UUID function is from this answer
+// https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/2117523#2117523
 export const uuidv4 = () => {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
